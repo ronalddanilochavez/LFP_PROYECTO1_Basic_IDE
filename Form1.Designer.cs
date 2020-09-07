d083452e96cbeab98a0597472f54ac2ebe8d4131
Danilo Chavez <48350233+ronalddanilochavez@users.noreply.github.com>
2020-09-07 05:29:02 -0600

    Inicio de GIt

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace LFP_PROYECTO1_Basic_IDE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FilesMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.crearProyectoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarProyectoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirProyectoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarProyectoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.crearArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.crearArchivoLogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.IDELexer = new System.Windows.Forms.RichTextBox();
+            this.RowColumn = new System.Windows.Forms.Label();
+            this.Log = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FilesMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1116, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FilesMenu
+            // 
+            this.FilesMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crearProyectoToolStripMenuItem,
+            this.eliminarProyectoToolStripMenuItem,
+            this.abrirProyectoToolStripMenuItem,
+            this.cerrarProyectoToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.crearArchivoToolStripMenuItem,
+            this.eliminarArchivoToolStripMenuItem,
+            this.abrirArchivoToolStripMenuItem,
+            this.cerrarArchivoToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.crearArchivoLogToolStripMenuItem,
+            this.toolStripMenuItem4,
+            this.cerrarToolStripMenuItem});
+            this.FilesMenu.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FilesMenu.Name = "FilesMenu";
+            this.FilesMenu.Size = new System.Drawing.Size(69, 21);
+            this.FilesMenu.Text = "Archivos";
+            // 
+            // crearProyectoToolStripMenuItem
+            // 
+            this.crearProyectoToolStripMenuItem.Name = "crearProyectoToolStripMenuItem";
+            this.crearProyectoToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.crearProyectoToolStripMenuItem.Text = "Crear Proyecto";
+            // 
+            // eliminarProyectoToolStripMenuItem
+            // 
+            this.eliminarProyectoToolStripMenuItem.Name = "eliminarProyectoToolStripMenuItem";
+            this.eliminarProyectoToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.eliminarProyectoToolStripMenuItem.Text = "Eliminar Proyecto";
+            // 
+            // abrirProyectoToolStripMenuItem
+            // 
+            this.abrirProyectoToolStripMenuItem.Name = "abrirProyectoToolStripMenuItem";
+            this.abrirProyectoToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.abrirProyectoToolStripMenuItem.Text = "Abrir Proyecto";
+            // 
+            // cerrarProyectoToolStripMenuItem
+            // 
+            this.cerrarProyectoToolStripMenuItem.Name = "cerrarProyectoToolStripMenuItem";
+            this.cerrarProyectoToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.cerrarProyectoToolStripMenuItem.Text = "Cerrar Proyecto";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(201, 22);
+            this.toolStripMenuItem2.Text = "-------------------------";
+            // 
+            // crearArchivoToolStripMenuItem
+            // 
+            this.crearArchivoToolStripMenuItem.Name = "crearArchivoToolStripMenuItem";
+            this.crearArchivoToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.crearArchivoToolStripMenuItem.Text = "Crear Archivo";
+            // 
+            // eliminarArchivoToolStripMenuItem
+            // 
+            this.eliminarArchivoToolStripMenuItem.Name = "eliminarArchivoToolStripMenuItem";
+            this.eliminarArchivoToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.eliminarArchivoToolStripMenuItem.Text = "Eliminar Archivo";
+            // 
+            // abrirArchivoToolStripMenuItem
+            // 
+            this.abrirArchivoToolStripMenuItem.Name = "abrirArchivoToolStripMenuItem";
+            this.abrirArchivoToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.abrirArchivoToolStripMenuItem.Text = "Abrir Archivo";
+            // 
+            // cerrarArchivoToolStripMenuItem
+            // 
+            this.cerrarArchivoToolStripMenuItem.Name = "cerrarArchivoToolStripMenuItem";
+            this.cerrarArchivoToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.cerrarArchivoToolStripMenuItem.Text = "Cerrar Archivo";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(201, 22);
+            this.toolStripMenuItem3.Text = "-------------------------";
+            // 
+            // crearArchivoLogToolStripMenuItem
+            // 
+            this.crearArchivoLogToolStripMenuItem.Name = "crearArchivoLogToolStripMenuItem";
+            this.crearArchivoLogToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.crearArchivoLogToolStripMenuItem.Text = "Crear Archivo Log";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(201, 22);
+            this.toolStripMenuItem4.Text = "-------------------------";
+            // 
+            // cerrarToolStripMenuItem
+            // 
+            this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.cerrarToolStripMenuItem.Text = "Cerrar";
+            // 
+            // IDELexer
+            // 
+            this.IDELexer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.IDELexer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDELexer.Location = new System.Drawing.Point(12, 43);
+            this.IDELexer.Name = "IDELexer";
+            this.IDELexer.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.IDELexer.Size = new System.Drawing.Size(1092, 285);
+            this.IDELexer.TabIndex = 1;
+            this.IDELexer.Text = "";
+            this.IDELexer.TextChanged += new System.EventHandler(this.IDELexer_TextChanged);
+            this.IDELexer.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.IDELexer_KeyPress);
+            // 
+            // RowColumn
+            // 
+            this.RowColumn.AutoSize = true;
+            this.RowColumn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RowColumn.Location = new System.Drawing.Point(12, 340);
+            this.RowColumn.Name = "RowColumn";
+            this.RowColumn.Size = new System.Drawing.Size(89, 16);
+            this.RowColumn.TabIndex = 2;
+            this.RowColumn.Text = "Fila, Columna";
+            // 
+            // Log
+            // 
+            this.Log.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Log.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Log.Location = new System.Drawing.Point(15, 373);
+            this.Log.Name = "Log";
+            this.Log.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.Log.Size = new System.Drawing.Size(1089, 147);
+            this.Log.TabIndex = 3;
+            this.Log.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 537);
+            this.Controls.Add(this.Log);
+            this.Controls.Add(this.RowColumn);
+            this.Controls.Add(this.IDELexer);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LFP_PROYECTO1_Basic_IDE - 200130586 Ronald Danilo Chávez Calderón";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FilesMenu;
+        private System.Windows.Forms.ToolStripMenuItem crearProyectoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarProyectoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirProyectoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarProyectoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem crearArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem crearArchivoLogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox IDELexer;
+        private System.Windows.Forms.Label RowColumn;
+        private System.Windows.Forms.RichTextBox Log;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LFP_PROYECTO1_Basic_IDE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static Color[] ParseString(string raw)
+        {
+            int len = 0;
+            foreach (char chr in raw)
+            {
+                len++;
+            }
+            Color[] ret = new Color[len];
+            string[] parsed = Regex.Split(raw, " ");
+            for (int i = 0; i < len; i++)
+            {
+                ret[i] = Color.Green;
+            }
+            return ret;
+        }
+
+        private void IDELexer_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*
+            // Check for an illegal character in the KeyDown event.
+            // Only this characters are all allowed
+            if (System.Text.RegularExpressions.Regex.IsMatch(e.KeyChar.ToString(), @"[^a-z^A-Z^0-9^+^\-^\/^\*^\(^\)^\>^\<^\!^\|^\&^\;^\=]"))
+            {
+                // Stop the character from being entered into the control since it is illegal.
+                e.Handled = true;
+            }
+            */
+        }
+
+        private void IDELexer_TextChanged(object sender, EventArgs e)
+        {
+            /*        
+            int pos = 0;
+            foreach (string ln in IDELexer.Lines)
+            {
+                Color[] Syntax = ParseString(ln);
+                int cur = 0;
+                foreach (char chr in ln)
+                {
+                    IDELexer.SelectionStart = pos;
+                    IDELexer.SelectionLength = 1;
+                    IDELexer.SelectionColor = Syntax[cur];
+                    cur++;
+                    pos++;
+                }
+            }*/
+        }
+    }
+}

95d5850fa9a22907a115cd665137256ed5e6773f
Danilo Chavez <48350233+ronalddanilochavez@users.noreply.github.com>
2020-09-14 22:28:01 -0600

    Add files via upload

---
+++ Automaton.cs
@@ -0,0 +1,65 @@
+﻿namespace LFP_PROYECTO1_Basic_IDE
+{
+    class Automaton
+    {
+        private string[] states { get; set;}
+
+        private string[] alphabet { get; set; }
+
+        private string[][] transitionFunction { get; set; }
+
+        private string initialState { get; set; }
+
+        private string[] finalStates { get; set; }
+
+        private string nextState (int actualState, int letter, string[][] transitionFunction)
+        {
+            string nextState = "";
+
+            if (transitionFunction.Length > 0)
+            {
+                for (int i = 0; i < transitionFunction.Length; i++)
+                {
+                    if (transitionFunction[i].Length > 0)
+                    {
+                        for (int j = 0; j < transitionFunction[i].Length; j++)
+                        {
+                            if (actualState == i && letter == j)
+                            {
+                                nextState = transitionFunction[i][j];
+                            }
+                        }
+                    }
+                }
+            }
+
+            return nextState;
+        }
+
+        public string AFD (string actualState, string letter, string[] acceptationStates)
+        {
+            string log = "";
+
+            string myNextState = "";
+            if (states.Length > 0)
+            {
+                for (int i = 0; i < states.Length; i++)
+                {
+                    if (states[i].Length > 0)
+                    {
+                        for (int j = 0; j < states.Length; i++)
+                        {
+                            if (states[i] == actualState && alphabet[j] == letter)
+                            {
+                                myNextState = nextState(i, j, transitionFunction);
+                                
+                            }
+                        }
+                    }
+                }
+            }
+
+            return log;
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -13,7 +14,10 @@ namespace LFP_PROYECTO1_Basic_IDE
 {
     public partial class Form1 : Form
     {
-        private MyLibraries myLibraries = new MyLibraries();
+        private IDE myIDE = new IDE();
+
+        // fileProject[0] is the project's file and fileProject[1] is the myIDE's file
+        private string[] fileProject = new string[2];
 
         private int letterPos = 0; 
         public Form1()
@@ -37,7 +41,7 @@ namespace LFP_PROYECTO1_Basic_IDE
             return ret;
         }
 
-        private void IDELexer_KeyPress(object sender, KeyPressEventArgs e)
+        private void myIDELexer_KeyPress(object sender, KeyPressEventArgs e)
         {
             /*
             // Check for an illegal character in the KeyDown event.
@@ -51,23 +55,91 @@ namespace LFP_PROYECTO1_Basic_IDE
         }
 
         /*
-        myLibraries.processFile(IDELexer.Text, IDELexer);
+        myIDE.processFile(myIDELexer.Text, myIDELexer);
 
-        RowColumn.Text = myLibraries.cursorRowPosition(IDELexer) + "," + myLibraries.cursorColumnPosition(IDELexer);
+        RowColumn.Text = myIDE.cursorRowPosition(myIDELexer) + "," + myIDE.cursorColumnPosition(myIDELexer);
         */
 
         private void IDELexer_TextChanged(object sender, EventArgs e)
         {
             // We need to asign the return of the function to some integer that counts the last colored index
-            letterPos = myLibraries.processText(IDELexer);
+            letterPos = myIDE.processText(IDELexer);
 
-            //myLibraries.processFile(IDELexer.Text, IDELexer);
+            //myIDE.processFile(myIDELexer.Text, myIDELexer);
 
             // To see the row and column
-            RowColumn.Text = myLibraries.cursorRowPosition(IDELexer) + "," + myLibraries.cursorColumnPosition(IDELexer);
+            RowColumn.Text = myIDE.cursorRowPosition(IDELexer) + "," + myIDE.cursorColumnPosition(IDELexer);
 
             // To log the tokens found
-            Log.Text = myLibraries.tokenList;
+            Log.Text = myIDE.tokenList;
         }
+
+        //********************************************************
+        //********************************************************
+        // Menu
+
+        private void crearProyectoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fileProject = myIDE.createFileProjectGTP(IDELexer);
+        }
+
+        private void abrirProyectoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fileProject = myIDE.openFileProjectGTP(IDELexer);
+        }
+
+        private void guardarProyectoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myIDE.saveFileProjectGTP(IDELexer, fileProject[0], fileProject[1]);
+            Log.AppendText("\nProyecto guardado exitosamente");
+        }
+
+        private void cerrarProyectoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IDELexer.Clear();
+            Log.Clear();
+        }
+
+        private void abrirArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myIDE.openFileGT(IDELexer);
+        }
+
+        private void guardarArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myIDE.saveFileGT(IDELexer);
+            Log.AppendText("\nArchivo guardado exitosamente");
+        }
+
+        private void cerrarArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IDELexer.Clear();
+            Log.Clear();
+        }
+
+        private void crearArchivoLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myIDE.saveFileLogGTE(Log);
+        }
+
+        private void abrirArchivoLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myIDE.openFileLogGTE(IDELexer);
+        }
+
+        private void cerrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //********************************************************
+        //********************************************************
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
